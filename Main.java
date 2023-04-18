b33cb7e21601087c51649a63679fb421a0ef5c02
Alessia Monteiro da Silva <72458974+alessiamonteiro@users.noreply.github.com>
2023-04-18 10:33:57 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,9 +3,8 @@ public class Main
 	public static void main(String[] args) {
 		Conta conta = new Conta(1234, 01, 1500);
 		System.out.println("saldo:"+ conta.saldo);
-		
+		conta.sacar(500);
+		conta.depositar(300);
 	}
-	
-	
-
 }
+
